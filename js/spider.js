fcb994c224ed9128eaa0bc68fc3deaf5e26ebb0a
zhulefengggg <zhulefenggg@163.com>
2023-10-30 16:20:37 +0800

    Site updated: 2023-10-30 16:20:34

--- js/spider.js
+++ js/spider.js
@@ -35,7 +35,7 @@ if (document.body.clientWidth > 992) {
                 right: 50 + 'px',
                 // 这里可以换为你喜欢的图片，例如我就换为了雪人，但是要抠图
                 nekoImg: "https://assetsblog.leoder.top/img/spider.webp",
-                hoverMsg: "I'm Spider-Man!",
+                hoverMsg: "Spider-Man!",
                 color: "#6f42c1",
                 during: 500,
                 blog_body: "body",
